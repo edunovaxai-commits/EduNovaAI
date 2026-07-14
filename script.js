182180f031a5b3b68456ec3c7f92c554bf83b2e3
edunovaxai-commits <edunovaxai@gmail.com>
2026-07-14 18:57:54 +0530

    Real Quiz Logic

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ function showMessage() {
 }
 function checkAnswer(){
 
+let ans=document.getElementById("correct");
+
+if(ans.checked){
+
 document.getElementById("result").innerHTML="✅ Correct Answer";
+
+}else{
+
+document.getElementById("result").innerHTML="❌ Wrong Answer";
+
+}
 
 }
