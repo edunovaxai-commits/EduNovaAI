573f7d3a5d380e68aefcf63d456583e49ca1ccdc
edunovaxai-commits <edunovaxai@gmail.com>
2026-07-15 22:59:23 +0530

    Add Mock Test System v1

--- script.js
+++ script.js
@@ -833,3 +833,26 @@ notes=`
 document.getElementById("notes").innerHTML = notes;
 
 }
+
+function checkQuiz(){
+
+let score=0;
+
+if(document.querySelector('input[name="q1"]:checked')?.value=="a"){
+score++;
+}
+
+if(document.querySelector('input[name="q2"]:checked')?.value=="b"){
+score++;
+}
+
+if(document.querySelector('input[name="q3"]:checked')?.value=="c"){
+score++;
+}
+
+document.getElementById("score").innerHTML =
+"🎉 Your Score: "+score+"/3";
+
+localStorage.setItem("quizScore",score);
+
+}
