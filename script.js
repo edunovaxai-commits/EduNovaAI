23f04d7f1a578860e9c04c8d9a5e2de7b006d547
edunovaxai-commits <edunovaxai@gmail.com>
2026-07-16 14:02:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -897,3 +897,28 @@ notes.style.display = "block";
 }
 
 }
+function toggleTheme(){
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        localStorage.setItem("theme","dark");
+
+    }else{
+
+        localStorage.setItem("theme","light");
+
+    }
+
+}
+
+window.onload = function(){
+
+    if(localStorage.getItem("theme")=="dark"){
+
+        document.body.classList.add("dark-mode");
+
+    }
+
+}
