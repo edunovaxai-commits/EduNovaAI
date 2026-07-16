9a720ecc4d30698438533475137c820373f5b6c7
edunovaxai-commits <edunovaxai@gmail.com>
2026-07-16 16:29:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -918,3 +918,24 @@ window.onload = function(){
     }
 
 }
+
+function saveProfile(){
+
+let name = document.getElementById("name").value.trim();
+let email = document.getElementById("email").value.trim();
+let mobile = document.getElementById("mobile").value.trim();
+
+if(name=="" || email=="" || mobile==""){
+    alert("Please fill all fields.");
+    return;
+}
+
+localStorage.setItem("studentName", name);
+localStorage.setItem("studentEmail", email);
+localStorage.setItem("studentMobile", mobile);
+
+alert("✅ Profile Updated Successfully!");
+
+window.location.href = "profile.html";
+
+}
